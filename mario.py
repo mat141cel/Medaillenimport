f61572ec2d83e6aedb55aebb828640c7fd47bf69
Mattia Celisi <134511870+mat141cel@users.noreply.github.com>
2024-05-16 19:23:28 +0000

    mariofinal

--- mario.py
+++ mario.py
@@ -2,10 +2,10 @@ from reusable_code import *
 import asyncio
 import re
 import argparse
+from tqdm import tqdm
 
-
-async def fetch_url(url, record_list):
-    response_text = make_request(url)
+def fetch_url(url):
+    response_text = requests.get(url).text
     root = ET.fromstring(response_text)
     # Define namespaces
     l = '{http://www.lido-schema.org}'
@@ -21,32 +21,53 @@ async def fetch_url(url, record_list):
         test = record.findall(f".//{l}object/{l}objectWebResource")
         nk_fundkomplex = ' | '.join([elem.text for elem in test if re.search(r'fundkomplex', elem.text)])
         ocre_crro_verknüpfung = ' | '.join([elem.text for elem in test if re.search(r'numismatics', elem.text)])
-        #print(nk_fundkomplex, ocre_crro_verknüpfung)
-
-
+      	
         # literatur ähnlich blöd wie fundkomplex
-        literaturfeld = record.findall(f".//{l}object/{l}objectNote")
-        nk_fundkomplex = ' | '.join([elem.text for elem in literaturfeld])
+        lit = record.findall(f".//{l}object/{l}objectNote")
+        # Assuming 'lit' is a list of elements
+        literaturfeld = ' | '.join(elem.text for i, elem in enumerate(lit[1:], 1) if lit[i-1].text == "Literatur")
 
-        record_list.append([handle_id, fundort, nk_fundkomplex, literaturfeld, ocre_crro_verknüpfung])
+        dat_begin = dat_ende = dat_verbal = None # initialize the variables
+        for event in record.findall(f".//{l}event"):
+            event_type = extr_text(event, f"{l}eventType/{l}term[2]")
+            if event_type == "Herstellung":
+                dat_begin = extr_text(event, f"{l}eventDate/{l}date/{l}earliestDate")
+                dat_ende = extr_text(event, f"{l}eventDate/{l}date/{l}latestDate")
+        try:
+            if "-" in dat_begin: dat_begin = dat_begin[:4]
+            if "-" in dat_ende: dat_ende = dat_ende[:4]
+            if int(dat_begin) < 750 or int(dat_ende) < 750:
+                records = {"handle_id": handle_id,
+                        "fundort": fundort,
+                        "nk_fundkomplex": nk_fundkomplex,
+                        "literaturfeld": literaturfeld,
+                        "ocre_crro_verknüpfung": ocre_crro_verknüpfung}
+                return records
+        except: pass
+        
 
 
-
-async def main():
+def main():
     # Opening JSON file
-    f = open('mario.json.json')
-    url_list = json.load(f)[:10]
-    print(F"number of medals to scrape: {len(url_list)}")
-    # Set the desired rate of requests per second
-    results = []
-    async def process_url(url):
-        await fetch_url(f"https://www.kenom.de/oai/?verb=GetRecord&metadataPrefix=lido&identifier={url}", results)
-    tasks = [process_url(url) for url in url_list]
-    await asyncio.gather(*tasks)
-    print(results)
-    save_json("output_mario", results)
+    with open('mario.json.json') as f: url_list = json.load(f)
+    batch_size = 1000
+    total_batches = (len(url_list) + batch_size - 1) // batch_size
 
-asyncio.run(main())
+    record_list = []  # List to store all fetched records
+    for i in range(0, len(url_list), batch_size):
+        batch = url_list[i : i + batch_size]
+        for _, url in tqdm(enumerate(batch), total=len(batch), desc=f"Fetching batch {i//batch_size + 1}"):
+            if r := fetch_url(f"https://www.kenom.de/oai/?verb=GetRecord&metadataPrefix=lido&identifier={url}"):
+                record_list.append(r)
+        print(f"Batch {i//batch_size + 1} completed. Total records fetched: {len(record_list)}")
+
+    # Optional: Save the results after each batch for extra safety
+    # Save the results after each batch for extra safety
+    print(record_list)
+    with open(f"mario_output.json", "w", encoding='utf-8') as f:
+        json.dump(record_list, f, ensure_ascii=False)
+
+main()
 
 async def process_oai():
     url_list = get_oai_urls("metadataPrefix=oai_dc&set=relation:fundkomplex:true")
